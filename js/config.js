8b3169a27d715d520235d736156b2f132b7b3172
Abhishek Verma <abhishekverma.av109@gmail.com>
2023-02-13 22:55:55 +0530

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_cy5aoRvWs', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'fi17lsqi650l1kb7s2lc2mque', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
